62b7cd687016e61b41b761b5771c0031f4bb777c
raiderlinda <49811957+raiderlinda@users.noreply.github.com>
2019-05-27 15:53:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "-"
-var adminprefix = '-'
-
-
-//bc
+var prefix = "-";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Amrica Shop ,!`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
 
 client.on("message", message => {
     if (message.content.startsWith("-obc")) {
