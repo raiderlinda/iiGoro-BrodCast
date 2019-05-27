0bc6a0d732f1c71d3e8208a00d533c0ef3213118
raiderlinda <49811957+raiderlinda@users.noreply.github.com>
2019-05-27 15:54:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,13 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "-";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Amrica Shop ,!`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
-});
+var prefix = "$"
+var adminprefix = '$'
+
+
+//bc
 
 client.on("message", message => {
-    if (message.content.startsWith("-obc")) {
+    if (message.content.startsWith("$obc")) {
                  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' ');
@@ -29,7 +23,7 @@ client.on("message", message => {
 //bc online
 
 
-  var prefix = "-";
+  var prefix = "$";
 
   client.on("message", message => {
   
@@ -72,7 +66,7 @@ m.sendMessage(args)
 });
 
   client.on('message', msg => {
-    if(msg.content === '-help')
+    if(msg.content === '$help')
     msg.reply('Check Your DM :white_check_mark:')
   });
   
@@ -96,6 +90,39 @@ m.sendMessage(args)
    }
    });
 
+const developers = ["472553752457641987","id"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
 
 
 client.login(process.env.TOKEN);
